9cd90b13bd83845ad26d38e70746c0278ca04317
avalynn <100826194+avalynndev@users.noreply.github.com>
2024-08-11 07:06:55 +0000

    update code

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        serverComponentsExternalPackages: ["@napi-rs/image"],
+    },
+};
 
 export default nextConfig;
--- webpack.config.ts
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  // ... other config options ...
-  resolve: {
-    alias: {
-      buffer: require.resolve('buffer/'),
-    },
-  },
-};
